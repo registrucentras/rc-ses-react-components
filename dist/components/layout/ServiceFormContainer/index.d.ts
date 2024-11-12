bc6e5cf937a6db4d61181535c70f300a6a346eb0
Vytautas Luksys <vytautas.luksys@cgi.com>
2024-11-12 08:12:48 +0200

    chore: library build

--- dist/components/layout/ServiceFormContainer/index.d.ts
+++ dist/components/layout/ServiceFormContainer/index.d.ts
@@ -1,3 +1,4 @@
+import { ContainerProps } from '@mui/system';
 import { default as React } from 'react';
 import { default as useAccordionController } from '../../common/Accordion/hooks/useAccordionController';
 type Props = {
@@ -5,6 +6,9 @@ type Props = {
     children: React.ReactNode;
     showAccordionCollapseControls?: boolean;
     showProgressStepper?: boolean;
+    slotProps?: {
+        container: Partial<ContainerProps>;
+    };
 };
-declare function ServiceFormContainer({ accordionController, children, showAccordionCollapseControls, showProgressStepper, }: Props): import("react/jsx-runtime").JSX.Element;
-export default ServiceFormContainer;
+declare function RcSesServiceFormContainer({ accordionController, children, showAccordionCollapseControls, showProgressStepper, slotProps, }: Props): import("react/jsx-runtime").JSX.Element;
+export default RcSesServiceFormContainer;
