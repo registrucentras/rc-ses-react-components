534cfb6237a3bdd218b8b7363f159352771d42b2
Vytautas Luksys <vytautas.luksys@cgi.com>
2025-02-28 10:42:17 +0200

    feat: MuiIcon theming

---
+++ src/theme/light/MuiSvgIcon.ts
@@ -0,0 +1,17 @@
+import { Components } from '@mui/material'
+
+const MuiSvgIcon: Components['MuiSvgIcon'] = {
+  styleOverrides: {
+    fontSizeSmall: {
+      fontSize: '1rem !important',
+    },
+    fontSizeMedium: {
+      fontSize: '1.5rem !important',
+    },
+    fontSizeLarge: {
+      fontSize: '2rem !important',
+    },
+  },
+}
+
+export default MuiSvgIcon
