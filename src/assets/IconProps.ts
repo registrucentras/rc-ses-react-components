dbce422b4f75daf5e407c7798a4cea465275fd71
daiski <dainius.skiparis@registrucentras.lt>
2026-04-14 15:32:26 +0300

    fix: add sx and aria-hidden to IconProps

--- src/assets/IconProps.ts
+++ src/assets/IconProps.ts
@@ -1,8 +1,12 @@
+import type { SxProps, Theme } from '@mui/material'
+
 type IconProps = {
+  'aria-hidden'?: boolean
   className?: string
   fillColor?: string
   size?: number
   strokeColor?: string
+  sx?: SxProps<Theme>
 }
 
 export default IconProps
