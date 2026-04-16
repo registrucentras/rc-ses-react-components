b86dc77a03fc37bfde3cec71981f8ce17590e47f
Your Name <bliudziust@gmail.com>
2026-04-16 14:45:54 +0300

    sandbox

---
+++ src/examples/CardMultipleStepForm/index.tsx
@@ -0,0 +1,44 @@
+import { Typography } from '@mui/material'
+
+import RcSesCardFormContainer from '@/components/layout/ServiceFormContainer/CardFormContainer'
+import ServiceHeader from '@/components/layout/ServiceHeader'
+import ServicePage from '@/components/layout/ServicePage'
+import { StepItem } from '@/components/layout/ServiceWizardStepper/StepperTypes'
+
+const steps: StepItem[] = [
+  { id: '1', state: 'completed', title: '1-2 žodžių tekstas' },
+  { id: '2', state: 'active', title: '1-2 žodžių tekstas' },
+  { id: '3', state: 'pending', title: '1-2 žodžių tekstas' },
+]
+
+function CardMultipleStepForm() {
+  return (
+    <ServicePage>
+      <ServiceHeader
+        breadcrumbsProps={{
+          path: [
+            { label: 'Pagrindinis', path: '/' },
+            {
+              label: 'Horizontalios formos su vedliu pavyzdys',
+              path: '/horizontal-sample-form-multiple-steps',
+            },
+          ],
+        }}
+        title='Nekilnojamojo turto registro išrašas pagal nurodytą turto adresą'
+      >
+        <Typography variant='body1'>
+          Šiame puslapyje Jūs galite užsisakyti Nekilnojamojo turto registro išrašus,
+          pažymas bei kitus dokumentus. Užsakytą NTR išrašą, pažymą ar kitą dokumentą
+          galėsite gauti elektroniniu būdu arba atsiimti pasirinktame VĮ Registrų centro
+          padalinyje.
+        </Typography>
+      </ServiceHeader>
+
+      <RcSesCardFormContainer steps={steps} title='Test' description='Test description'>
+        <Typography variant='body1'>Test Body Text</Typography>
+      </RcSesCardFormContainer>
+    </ServicePage>
+  )
+}
+
+export default CardMultipleStepForm
