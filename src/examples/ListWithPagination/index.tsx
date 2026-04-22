27d3e1a522d6b7e963e164988ed19bbc3ec8e077
Your Name <bliudziust@gmail.com>
2026-04-22 14:23:02 +0300

    sandbox update to mock data loading

--- src/examples/ListWithPagination/index.tsx
+++ src/examples/ListWithPagination/index.tsx
@@ -1,10 +1,76 @@
 import { Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 
+import Button from '@/components/common/Button'
 import SelectableCardList from '@/components/common/SelectableCardList'
 import ServiceHeader from '@/components/layout/ServiceHeader'
 import ServicePage from '@/components/layout/ServicePage'
+import { QuestionIcon } from '@/library'
+import palette from '@/theme/palette'
+
+const mockListItems = [
+  {
+    id: '1',
+    icon: <QuestionIcon fillColor={palette.grey[600]} />,
+    text: 'Explanatory text Test ilgas pavadinimas',
+    tooltip: 'Extra info',
+  },
+  {
+    id: '2',
+    icon: <QuestionIcon fillColor={palette.grey[600]} />,
+    text: 'Disabled item',
+    tooltip: 'Extra info',
+    disabled: true,
+  },
+  {
+    id: '3',
+    icon: <QuestionIcon fillColor={palette.grey[600]} />,
+    text: 'Hover me',
+    tooltip: 'Extra info',
+  },
+  {
+    id: '4',
+    icon: <QuestionIcon fillColor={palette.grey[600]} />,
+    text: 'Loading item',
+    tooltip: 'Extra info',
+  },
+]
+
+const mockItems = [
+  {
+    id: '1',
+    name: 'Item 1',
+    matchedName: 'Additional info 1',
+    listItems: mockListItems,
+  },
+  {
+    id: '2',
+    name: 'Item 2',
+    listItems: mockListItems,
+  },
+  {
+    id: '3',
+    name: 'Item 3',
+    matchedName: 'Additional info 3',
+    listItems: mockListItems,
+  },
+]
 
 function ListWithPagination() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState<typeof mockItems | undefined>(undefined)
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const timer = setTimeout(() => {
+      setData(mockItems)
+      setIsLoading(false)
+    }, 1500)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <ServicePage>
       <ServiceHeader
@@ -26,7 +92,21 @@ function ListWithPagination() {
           padalinyje.
         </Typography>
       </ServiceHeader>
-      <SelectableCardList />
+      <SelectableCardList items={data} isLoading={isLoading} />
+      <Button
+        variant='contained'
+        onClick={() => {
+          setIsLoading(true)
+          setData(undefined)
+
+          setTimeout(() => {
+            setData(mockItems)
+            setIsLoading(false)
+          }, 1500)
+        }}
+      >
+        Refetch
+      </Button>
     </ServicePage>
   )
 }
