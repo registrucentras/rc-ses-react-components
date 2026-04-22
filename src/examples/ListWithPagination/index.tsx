b1bb7af971f8db2332421c57b713e1170d741ce4
Your Name <bliudziust@gmail.com>
2026-04-22 11:15:13 +0300

    sandbox

--- src/examples/ListWithPagination/index.tsx
+++ src/examples/ListWithPagination/index.tsx
@@ -1,72 +1,32 @@
-import { Button, Grid } from '@mui/material'
-import React, { useState } from 'react'
+import { Typography } from '@mui/material'
 
-import DataPagination from '@/components/common/DataPagination'
-import ListWithIcons from '@/components/common/ListWithIcons'
+import SelectableCardList from '@/components/common/SelectableCardList'
+import ServiceHeader from '@/components/layout/ServiceHeader'
 import ServicePage from '@/components/layout/ServicePage'
-import { QuestionIcon } from '@/library/icons'
-import palette from '@/theme/palette'
-
-const mockItems = [
-  {
-    id: '1',
-    icon: <QuestionIcon fillColor={palette.grey[600]} />,
-    text: 'Explanatory text Test ilgas pavadinimas',
-    tooltip: 'Extra info',
-  },
-  {
-    id: '2',
-    icon: <QuestionIcon fillColor={palette.grey[600]} />,
-    text: 'Disabled item',
-    tooltip: 'Extra info',
-    disabled: true,
-  },
-  {
-    id: '3',
-    icon: <QuestionIcon fillColor={palette.grey[600]} />,
-    text: 'Hover me',
-    tooltip: 'Extra info',
-  },
-  {
-    id: '4',
-    icon: <QuestionIcon fillColor={palette.grey[600]} />,
-    text: 'Loading item',
-    tooltip: 'Extra info',
-  },
-]
 
 function ListWithPagination() {
-  const [page, setPage] = useState(1)
-  const [isLoading, setIsLoading] = useState(false)
-  const ITEMS_PER_PAGE = 5
-
-  const mockData = React.useMemo(
-    () =>
-      Array.from({ length: 42 }, (_, i) => ({
-        id: i + 1,
-        name: `Item ${i + 1}`,
-      })),
-    [],
-  )
-
-  const handleSetIsLoading = () => {
-    setIsLoading(!isLoading)
-  }
-
   return (
     <ServicePage>
-      <Grid container sx={{ mt: 2 }}>
-        <ListWithIcons items={mockItems} layout='horizontal' isLoading={isLoading} />
-      </Grid>
-
-      <DataPagination
-        count={Math.ceil(mockData.length / ITEMS_PER_PAGE)}
-        page={page}
-        onChange={setPage}
-      />
-      <Button onClick={handleSetIsLoading} sx={{ mt: 2 }}>
-        Toggle loading
-      </Button>
+      <ServiceHeader
+        breadcrumbsProps={{
+          path: [
+            { label: 'Pagrindinis', path: '/' },
+            {
+              label: 'Horizontalios formos su vedliu pavyzdys',
+              path: '/horizontal-sample-form-multiple-steps',
+            },
+          ],
+        }}
+        title='Nekilnojamojo turto registro išrašas pagal nurodytą turto adresą'
+      >
+        <Typography variant='body1'>
+          Šiame puslapyje Jūs galite užsisakyti Nekilnojamojo turto registro išrašus,
+          pažymas bei kitus dokumentus. Užsakytą NTR išrašą, pažymą ar kitą dokumentą
+          galėsite gauti elektroniniu būdu arba atsiimti pasirinktame VĮ Registrų centro
+          padalinyje.
+        </Typography>
+      </ServiceHeader>
+      <SelectableCardList />
     </ServicePage>
   )
 }
