3587d0cb82b3024b8be6059132a28bcb671db1c7
saukaite <Sandra.Saukaite@registrucentras.lt>
2026-04-23 16:31:42 +0300

    SAV-5284: add Tab component to the Storybook

---
+++ src/stories/components/navigation/Tab.stories.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/function-component-definition */
+import { Meta, StoryObj } from '@storybook/react'
+
+import RcSesTab from '@/components/common/Tabs/Tab'
+
+const meta: Meta<typeof RcSesTab> = {
+  title: 'components/navigation/Tab',
+  component: RcSesTab,
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: { control: 'boolean' },
+    label: { control: 'text' },
+    icon: { control: 'text' },
+    iconPosition: {
+      control: { type: 'select', options: ['top', 'bottom', 'start', 'end'] },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: { label: 'Tab Label' },
+}
+
+export const WithIcon: Story = {
+  args: { label: 'Home', icon: '⭐', iconPosition: 'start' },
+}
+
+export const Disabled: Story = {
+  args: { label: 'Disabled Tab', disabled: true },
+}
