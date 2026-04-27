1f407c923eba341a2b668a50c9f509b87271efff
Tomas B. <tomas.bagdonas@registrucentras.lt>
2026-04-27 11:46:17 +0300

    Fix: export ButtonProps type in library types (#70)
    * Fix: export ButtonProps type in library types
    
    * version bump
    
    * add check-dist script
    
    * remove unused ESLint overrides and update ignore patterns

--- src/library/types.ts
+++ src/library/types.ts
@@ -5,7 +5,9 @@ import {
 } from '@/components/common/ListWithIcons'
 import { RcSesCardFormContainerProps } from '@/components/layout/ServiceFormContainer/CardFormContainer'
 import { ServiceWizardStepperProps } from '@/components/layout/ServiceWizardStepper'
+import { ButtonProps } from '@/types/buttons/ButtonProps'
 
 export type { ListWithIconsProps, ListWithIconsItemData }
+export type { ButtonProps }
 
 export type { RcSesCardFormContainerProps, RcSesCardProps, ServiceWizardStepperProps }
