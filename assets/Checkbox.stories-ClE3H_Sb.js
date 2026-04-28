df898d593ff7627c4b5f6d3abdc6aa565927b0b4
tomas562 <tomas562@users.noreply.github.com>
2026-04-28 10:31:43 +0000

    deploy: 75bfe507e80bda5f211bf6166386448349908362

---
+++ assets/Checkbox.stories-ClE3H_Sb.js
@@ -0,0 +1,79 @@
+import{j as a}from"./jsx-runtime-CUA6FXPC.js";import{u as A}from"./index.esm-DTUFWLrQ.js";import{r as g}from"./index-z7qk6wml.js";import{v as x,R as C}from"./index-CQqtGR1S.js";import{R as k}from"./CheckboxFormControl-DlQbEvZU.js";import{F as u}from"./FieldPreviewRow-BQCizoe8.js";import{F as R}from"./FieldView-BPNWESYs.js";import{F as S}from"./Fields-CoUdH7oK.js";import{P as p}from"./PreviewTitle-C1rB_Xe4.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./useTranslation-DpB118ej.js";import"./index-0qEeqX2c.js";import"./DefaultPropsProvider-C85juv10.js";import"./palette-DW5JBtDV.js";import"./WarningDiamondFillIcon-qcDNV13N.js";import"./IconBase.es-DjmddrNl.js";import"./CheckCircleFillIcon-BEND09qK.js";import"./InfoFillIcon-Dk6F1lNO.js";import"./WarningFillIcon-BkZRbW9g.js";import"./colorManipulator-DnbABVGE.js";import"./Box-Bw9i7Bet.js";import"./styled-B7PEox7f.js";import"./useMediaQuery-B3sOGpc7.js";import"./getThemeProps-CY3DWdQx.js";import"./TransitionGroupContext-CFTfohW9.js";import"./FormControl-BAZBwVJz.js";import"./useFormControl-DANjLPU6.js";import"./isMuiElement-CYARxs4c.js";import"./FormLabel-PNDU5E6u.js";import"./formControlState-Dq1zat_P.js";import"./SpinnerGapBoldIcon-BM5I0yo-.js";import"./FormControlLabel-WGyVMdv7.js";import"./Typography-EOcFdSjP.js";import"./Stack-CifLv3VS.js";import"./useThemeProps-CVqq43G5.js";import"./SwitchBase-B8RmCvcx.js";import"./useControlled-B5uRKyCK.js";import"./ButtonBase-C8XVWg5R.js";import"./useIsFocusVisible-CTcnAXki.js";import"./createSvgIcon-dN7utv6K.js";function t(){var r="/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/components/form/inputs/Checkbox.tsx",n="311c77e51c6b4f7fb99cc898ca17c75e136df959",e=window,l="__coverage__",s={path:"/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/components/form/inputs/Checkbox.tsx",statementMap:{0:{start:{line:7,column:39},end:{line:7,column:44}},1:{start:{line:8,column:13},end:{line:8,column:60}},2:{start:{line:8,column:27},end:{line:8,column:47}},3:{start:{line:9,column:2},end:{line:18,column:4}},4:{start:{line:21,column:0},end:{line:27,column:50}},5:{start:{line:23,column:4},end:{line:23,column:48}},6:{start:{line:25,column:4},end:{line:25,column:2087}}},fnMap:{0:{name:"RcSesCheckbox",decl:{start:{line:6,column:9},end:{line:6,column:22}},loc:{start:{line:6,column:30},end:{line:19,column:1}},line:6},1:{name:"(anonymous_1)",decl:{start:{line:8,column:21},end:{line:8,column:22}},loc:{start:{line:8,column:27},end:{line:8,column:47}},line:8}},branchMap:{0:{loc:{start:{line:8,column:27},end:{line:8,column:47}},type:"binary-expr",locations:[{start:{line:8,column:27},end:{line:8,column:35}},{start:{line:8,column:39},end:{line:8,column:47}}],line:8}},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0},f:{0:0,1:0},b:{0:[0,0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/components/form/inputs/Checkbox.tsx"],names:[],mappings:"AA8CM;AA7CN;AAEA;AAEA;AAGA;AAyBA;AACE;AAGA;AAEA;AACE;AAAC;AAAA;AACC;AACA;AAC0B;AACX;AAE8B;AAAA;AAGnD;AAEA;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"311c77e51c6b4f7fb99cc898ca17c75e136df959"},i=e[l]||(e[l]={});(!i[r]||i[r].hash!==n)&&(i[r]=s);var d=i[r];return t=function(){return d},d}t();function o(r){var i;t().f[0]++;const{errors:n,label:e,slotProps:l}=(t().s[0]++,r),s=(t().s[1]++,g.useMemo(()=>(t().f[1]++,t().s[2]++,t().b[0][0]++,r.id??(t().b[0][1]++,x())),[r.id]));return t().s[3]++,a.jsx(C,{errors:n,label:e,required:!!((i=r==null?void 0:r.rules)!=null&&i.required),...l==null?void 0:l.wrapper,children:a.jsx(k,{...r,id:s})})}t().s[4]++;try{t().s[5]++,o.displayName="RcSesCheckbox",t().s[6]++,o.__docgenInfo={description:"",displayName:"RcSesCheckbox",props:{control:{defaultValue:null,description:"",name:"control",required:!1,type:{name:"Control<any>"}},rules:{defaultValue:null,description:"",name:"rules",required:!1,type:{name:'Omit<RegisterOptions<any, any>, "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs">'}},onChange:{defaultValue:null,description:"Callback fired when the state is changed.\n@param event The event source of the callback.\nYou can pull out the new checked state by accessing `event.target.checked` (boolean).",name:"onChange",required:!1,type:{name:"((event: ChangeEvent<HTMLInputElement>, checked: boolean) => void)"}},disabled:{defaultValue:{value:"false"},description:"If `true`, the component is disabled.",name:"disabled",required:!1,type:{name:"boolean"}},name:{defaultValue:null,description:"Name attribute of the `input` element.",name:"name",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"",name:"label",required:!1,type:{name:"ReactNode"}},errors:{defaultValue:null,description:"",name:"errors",required:!1,type:{name:"FieldError"}},id:{defaultValue:null,description:"",name:"id",required:!1,type:{name:"string"}},loading:{defaultValue:null,description:"",name:"loading",required:!1,type:{name:"boolean"}},variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"flat"'},{value:'"outlined"'}]}},slotProps:{defaultValue:null,description:"",name:"slotProps",required:!1,type:{name:"{ field?: Partial<Omit<CheckboxProps, ImmediateFieldProps>>; label?: Partial<FormControlLabelProps>; wrapper?: Partial<...> | undefined; } | undefined"}}}}}catch{}const b=["outlined","flat"],c="Lorem Ipsum has been the standard.",pe={title:"components/inputs/Checkbox",component:o,argTypes:{variant:{options:b,mapping:b,table:{defaultValue:{summary:"flat"}}},control:{table:{disable:!0}},rules:{table:{disable:!0}},name:{table:{disable:!0}},slotProps:{table:{disable:!0}},loading:{table:{disable:!0}},id:{table:{disable:!0}},onChange:{table:{disable:!0}},errors:{table:{disable:!0}}},tags:["autodocs"]},w=r=>{const{control:n,formState:{errors:e}}=A({mode:"all",defaultValues:{agreement:""}}),{label:l,disabled:s,variant:i}=r;return a.jsxs(S,{children:[a.jsx(R,{children:a.jsx(o,{id:"agreement",name:"agreement",control:n,errors:e==null?void 0:e.agreement,label:l,disabled:s,variant:i,rules:{required:!0},children:c})}),a.jsxs(u,{children:[a.jsx(p,{children:"State previews outlined"}),a.jsx(o,{id:"active",name:"active",control:n,errors:e==null?void 0:e.agreement,label:"Active",rules:{required:!0},children:c}),a.jsx(o,{id:"disabled",name:"disabled",control:n,errors:e==null?void 0:e.agreement,label:"Disabled",rules:{required:!0},disabled:!0,children:c}),a.jsx(o,{id:"error",name:"error",control:n,errors:{message:"Error Message",type:"required"},label:"Error",rules:{required:!0},children:c})]}),a.jsxs(u,{children:[a.jsx(p,{children:"State previews flat"}),a.jsx(o,{id:"flatActive",name:"flatActive",control:n,errors:e==null?void 0:e.agreement,label:"Active",variant:"flat",children:c}),a.jsx(o,{id:"flatDisabled",name:"flatDisabled",control:n,errors:e==null?void 0:e.agreement,label:"Disabled",variant:"flat",children:c}),a.jsx(o,{id:"flatError",name:"flatError",control:n,errors:e==null?void 0:e.agreement,label:"Error",variant:"flat",children:c})]})]})},y=r=>{const{variant:n,label:e,children:l,disabled:s}=r;return`
+  import RcSesCheckbox from '@/components/form/inputs/Checkbox'
+
+  const MyComponent = () => (
+
+  const {control,formState: { errors }} = useForm<FormModel>({mode: 'all', defaultValues: {agreement: ''}})
+
+    <RcSesCheckbox
+          id='agreement'
+          name='agreement'
+          control={control}
+          errors={errors?.agreement}
+          label="${e}"
+          variant="${n}"
+          children="${l}"
+          disabled="${s}"
+        />
+  );`},m=w.bind({});m.args={label:"Click Me",variant:"outlined",children:"This is content",disabled:!1};m.parameters={docs:{source:{type:"dynamic",transform:(r,n)=>y(n.args)}}};var f,h,v;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`args => {
+  const {
+    control,
+    formState: {
+      errors
+    }
+  } = useForm<FormModel>({
+    mode: 'all',
+    defaultValues: {
+      agreement: ''
+    }
+  });
+  const {
+    label,
+    disabled,
+    variant
+  } = args;
+  return <Fields>
+      <FieldView>
+        <RcSesCheckbox id='agreement' name='agreement' control={control} errors={errors?.agreement} label={label} disabled={disabled} variant={variant} rules={{
+        required: true
+      }}>
+          {lorem}
+        </RcSesCheckbox>
+      </FieldView>
+
+      <FieldPreviewRow>
+        <PreviewTitle>State previews outlined</PreviewTitle>
+        <RcSesCheckbox id='active' name='active' control={control} errors={errors?.agreement} label='Active' rules={{
+        required: true
+      }}>
+          {lorem}
+        </RcSesCheckbox>
+        <RcSesCheckbox id='disabled' name='disabled' control={control} errors={errors?.agreement} label='Disabled' rules={{
+        required: true
+      }} disabled>
+          {lorem}
+        </RcSesCheckbox>
+        <RcSesCheckbox id='error' name='error' control={control} errors={{
+        message: 'Error Message',
+        type: 'required'
+      }} label='Error' rules={{
+        required: true
+      }}>
+          {lorem}
+        </RcSesCheckbox>
+      </FieldPreviewRow>
+      <FieldPreviewRow>
+        <PreviewTitle>State previews flat</PreviewTitle>
+        <RcSesCheckbox id='flatActive' name='flatActive' control={control} errors={errors?.agreement} label='Active' variant='flat'>
+          {lorem}
+        </RcSesCheckbox>
+        <RcSesCheckbox id='flatDisabled' name='flatDisabled' control={control} errors={errors?.agreement} label='Disabled' variant='flat'>
+          {lorem}
+        </RcSesCheckbox>
+        <RcSesCheckbox id='flatError' name='flatError' control={control} errors={errors?.agreement} label='Error' variant='flat'>
+          {lorem}
+        </RcSesCheckbox>
+      </FieldPreviewRow>
+    </Fields>;
+}`,...(v=(h=m.parameters)==null?void 0:h.docs)==null?void 0:v.source}}};const be=["Main"];export{m as Main,be as __namedExportsOrder,pe as default};
+//# sourceMappingURL=Checkbox.stories-ClE3H_Sb.js.map
