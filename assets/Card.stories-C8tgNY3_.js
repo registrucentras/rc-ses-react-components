1ea37c5caf977e556c7c096dd5eb1e557a3aa2a1
tomas562 <tomas562@users.noreply.github.com>
2026-05-05 12:30:31 +0000

    deploy: 4fbc68bc61255b36282450756127f729e4180f73

---
+++ assets/Card.stories-C8tgNY3_.js
@@ -0,0 +1,55 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{C}from"./CaretLeftIcon-B-8nUByR.js";import{M as E}from"./MagnifyingGlassIcon-CQ5ISzoX.js";import{r as e}from"./index-CqJgnuY1.js";import{p as R}from"./IconBase.es-CLqjCiWW.js";import{R as o}from"./index-D0Dxmr5l.js";import{R as Z}from"./index-CViX7lW2.js";import{a as g}from"./palette-DnsgEXz1.js";import{B as S}from"./Box-CscsmZ--.js";import{T as j}from"./Typography-KNMXKf19.js";import"./index-yBjzXJbu.js";import"./CaretLeft.es-CdHrrt0k.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./Button-BcEHCyBj.js";import"./createTheme-ClYpPn9r.js";import"./generateUtilityClasses-B2ID6taa.js";import"./styled-wPX_n2pi.js";import"./defaultTheme-Ca0qI0zY.js";import"./DefaultPropsProvider-DlaphqLI.js";import"./composeClasses-fLhin0tj.js";import"./ButtonBase-Bz6qKCDR.js";import"./TransitionGroupContext-9aFOjLlw.js";import"./useIsFocusVisible-2h147_XD.js";import"./Card-Be7rIwkx.js";import"./Paper-BJpnW-nv.js";import"./Stack-C-eIlwtW.js";import"./useThemeProps-CSNa12iN.js";import"./getThemeProps-DrzoWQ6J.js";import"./useTheme-CmrYzTbF.js";const w=new Map([["bold",e.createElement(e.Fragment,null,e.createElement("path",{d:"M230.15,70.54,185.46,25.86a20,20,0,0,0-28.28,0L33.86,149.17A19.86,19.86,0,0,0,28,163.31V208a20,20,0,0,0,20,20H216a12,12,0,0,0,0-24H125L230.15,98.83A20,20,0,0,0,230.15,70.54ZM91,204H52V165l84-84,39,39ZM192,103,153,64l18.34-18.34,39,39Z"}))],["duotone",e.createElement(e.Fragment,null,e.createElement("path",{d:"M221.66,90.34,192,120,136,64l29.66-29.66a8,8,0,0,1,11.31,0L221.66,79A8,8,0,0,1,221.66,90.34Z",opacity:"0.2"}),e.createElement("path",{d:"M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM48,163.31l88-88L180.69,120l-88,88H48Zm144-54.62L147.32,64l24-24L216,84.69Z"}))],["fill",e.createElement(e.Fragment,null,e.createElement("path",{d:"M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM192,108.69,147.32,64l24-24L216,84.69Z"}))],["light",e.createElement(e.Fragment,null,e.createElement("path",{d:"M225.91,74.79,181.22,30.1a14,14,0,0,0-19.8,0L38.1,153.41a13.94,13.94,0,0,0-4.1,9.9V208a14,14,0,0,0,14,14H216a6,6,0,0,0,0-12H110.49L225.91,94.59A14,14,0,0,0,225.91,74.79ZM93.52,210H48a2,2,0,0,1-2-2V163.31a2,2,0,0,1,.59-1.41L136,72.49,183.52,120ZM217.42,86.1,192,111.52,144.49,64,169.9,38.59a2,2,0,0,1,2.83,0l44.69,44.68A2,2,0,0,1,217.42,86.1Z"}))],["regular",e.createElement(e.Fragment,null,e.createElement("path",{d:"M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.69,147.32,64l24-24L216,84.69Z"}))],["thin",e.createElement(e.Fragment,null,e.createElement("path",{d:"M224.49,76.2,179.8,31.51a12,12,0,0,0-17,0L39.51,154.83A12,12,0,0,0,36,163.31V208a12,12,0,0,0,12,12H216a4,4,0,0,0,0-8H105.66L224.49,93.17A12,12,0,0,0,224.49,76.2ZM94.34,212H48a4,4,0,0,1-4-4V163.31a4,4,0,0,1,1.17-2.82L136,69.66,186.35,120ZM218.83,87.51,192,114.34,141.66,64l26.83-26.83a4,4,0,0,1,5.66,0l44.68,44.69A4,4,0,0,1,218.83,87.51Z"}))]]),I=e.forwardRef((t,r)=>e.createElement(R,{ref:r,...t,weights:w}));I.displayName="PencilSimpleLineIcon";function a(){var t="/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/assets/icons/PencilSimpleLineIcon.tsx",r="ac823ee0ce45194f2f18a16b22f9c4e9e5856088",i=window,l="__coverage__",H={path:"/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/assets/icons/PencilSimpleLineIcon.tsx",statementMap:{0:{start:{line:4,column:2},end:{line:13,column:4}},1:{start:{line:16,column:0},end:{line:22,column:50}},2:{start:{line:18,column:4},end:{line:18,column:62}},3:{start:{line:20,column:4},end:{line:20,column:659}}},fnMap:{0:{name:"PencilSimpleLineIcon",decl:{start:{line:3,column:9},end:{line:3,column:29}},loc:{start:{line:3,column:76},end:{line:14,column:1}},line:3}},branchMap:{0:{loc:{start:{line:3,column:54},end:{line:3,column:63}},type:"default-arg",locations:[{start:{line:3,column:61},end:{line:3,column:63}}],line:3}},s:{0:0,1:0,2:0,3:0},f:{0:0},b:{0:[0]},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/rc-ses-react-components/rc-ses-react-components/src/assets/icons/PencilSimpleLineIcon.tsx"],names:[],mappings:"AAMI;AANJ;AAIA;AACE;AACE;AAAC;AAAA;AACK;AACJ;AACA;AACO;AACA;AAAA;AAGb;AAEA;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"ac823ee0ce45194f2f18a16b22f9c4e9e5856088"},s=i[l]||(i[l]={});(!s[t]||s[t].hash!==r)&&(s[t]=H);var u=s[t];return a=function(){return u},u}a();function d({className:t,fillColor:r,size:i=(a().b[0][0]++,24),...l}){return a().f[0]++,a().s[0]++,n.jsx(I,{...l,className:t,size:i,weight:"fill",color:r})}a().s[1]++;try{a().s[2]++,d.displayName="PencilSimpleLineIcon",a().s[3]++,d.__docgenInfo={description:"",displayName:"PencilSimpleLineIcon",props:{fillColor:{defaultValue:null,description:"",name:"fillColor",required:!1,type:{name:"string"}},size:{defaultValue:{value:"24"},description:"",name:"size",required:!1,type:{name:"number"}},weight:{defaultValue:null,description:"",name:"weight",required:!1,type:{name:"enum",value:[{value:'"light"'},{value:'"fill"'},{value:'"thin"'},{value:'"regular"'},{value:'"bold"'},{value:'"duotone"'}]}}}}}catch{}const se={title:"components/display/Card",component:Z,tags:["autodocs"]},c={args:{title:"Pavadinimas",description:"Papildomas aprasymo tekstas",children:n.jsx(S,{sx:{alignItems:"center",color:"text.primary",display:"flex",justifyContent:"center",minHeight:{xs:"9.625rem",md:"11.5rem"},px:3,width:"100%"},children:n.jsx(j,{align:"center",children:"Vidinis paslaugu front-end sprendimas"})}),leadingActions:n.jsx(o,{startIcon:n.jsx(C,{}),color:"grey",variant:"outlined",children:"Button"}),trailingActions:n.jsxs(n.Fragment,{children:[n.jsx(o,{color:"grey",variant:"outlined",children:"Button"}),n.jsx(o,{children:"Button"})]})}},m={args:{title:"Juridinis asmuo",description:'UAB "Telia Lietuva", 121215434',contentBackground:!1,headerAction:n.jsx(o,{startIcon:n.jsx(d,{}),variant:"text",size:"small",children:"Redaguoti"})}},p={args:{variant:"elevation",centered:!0,title:"Place heading text here",description:"Additional description text elaborating on situation and what to do next.",image:n.jsx(S,{sx:{color:g.grey[400],position:"relative",display:"inline-flex"},children:n.jsx(E,{size:64,fillColor:g.grey[400]})}),children:n.jsx(o,{children:"Button"}),contentBackground:!1}};var A,f,h;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    title: 'Pavadinimas',
+    description: 'Papildomas aprasymo tekstas',
+    children: <Box sx={{
+      alignItems: 'center',
+      color: 'text.primary',
+      display: 'flex',
+      justifyContent: 'center',
+      minHeight: {
+        xs: '9.625rem',
+        md: '11.5rem'
+      },
+      px: 3,
+      width: '100%'
+    }}>
+        <Typography align='center'>Vidinis paslaugu front-end sprendimas</Typography>
+      </Box>,
+    leadingActions: <RcSesButton startIcon={<CaretLeftIcon />} color='grey' variant='outlined'>
+        Button
+      </RcSesButton>,
+    trailingActions: <>
+        <RcSesButton color='grey' variant='outlined'>
+          Button
+        </RcSesButton>
+        <RcSesButton>Button</RcSesButton>
+      </>
+  }
+}`,...(h=(f=c.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var x,v,y;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    title: 'Juridinis asmuo',
+    description: 'UAB "Telia Lietuva", 121215434',
+    contentBackground: false,
+    headerAction: <RcSesButton startIcon={<PencilSimpleLineIcon />} variant='text' size='small'>
+        Redaguoti
+      </RcSesButton>
+  }
+}`,...(y=(v=m.parameters)==null?void 0:v.docs)==null?void 0:y.source}}};var B,L,M;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    variant: 'elevation',
+    centered: true,
+    title: 'Place heading text here',
+    description: 'Additional description text elaborating on situation and what to do next.',
+    image: <Box sx={{
+      color: palette.grey[400],
+      position: 'relative',
+      display: 'inline-flex'
+    }}>
+        <MagnifyingGlassIcon size={64} fillColor={palette.grey[400]} />
+      </Box>,
+    children: <RcSesButton>Button</RcSesButton>,
+    contentBackground: false
+  }
+}`,...(M=(L=p.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};const ce=["Main","WithHeaderAction","WithImage"];export{c as Main,m as WithHeaderAction,p as WithImage,ce as __namedExportsOrder,se as default};
+//# sourceMappingURL=Card.stories-C8tgNY3_.js.map
---
+++ assets/CheckboxFormControl.stories-DShwzYB3.js
@@ -0,0 +1,41 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{u as d}from"./index.esm-CYp9Cab2.js";import{R as p}from"./CheckboxFormControl-BebIBdmT.js";import{F as b}from"./FieldView-BeHDZRgt.js";import{F as u}from"./Fields-Bvx7BN71.js";import"./index-yBjzXJbu.js";import"./index-CqJgnuY1.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./CheckBoldIcon-Bc2khP1c.js";import"./IconBase.es-CLqjCiWW.js";import"./CheckUncheckedBoldIcon-Bdh5zq_V.js";import"./SpinnerGapBoldIcon-DWHsf0Xa.js";import"./palette-DnsgEXz1.js";import"./FormControlLabel-BxHLDVOO.js";import"./createTheme-ClYpPn9r.js";import"./generateUtilityClasses-B2ID6taa.js";import"./styled-wPX_n2pi.js";import"./defaultTheme-Ca0qI0zY.js";import"./formControlState-Dq1zat_P.js";import"./DefaultPropsProvider-DlaphqLI.js";import"./useFormControl-xtefzQVn.js";import"./Typography-KNMXKf19.js";import"./composeClasses-fLhin0tj.js";import"./Stack-C-eIlwtW.js";import"./useThemeProps-CSNa12iN.js";import"./getThemeProps-DrzoWQ6J.js";import"./useTheme-CmrYzTbF.js";import"./FormLabel-BPYdDIQH.js";import"./Checkbox-OyRjl7bw.js";import"./SwitchBase-CKbkoNgJ.js";import"./useControlled-DKQkVSHg.js";import"./ButtonBase-Bz6qKCDR.js";import"./TransitionGroupContext-9aFOjLlw.js";import"./useIsFocusVisible-2h147_XD.js";import"./createSvgIcon-ikBOYRqt.js";const s=["outlined","flat"],Y={title:"components/form/CheckboxFormControl",component:p,argTypes:{variant:{options:s,mapping:s,table:{defaultValue:{summary:"outlined"}}},slotProps:{table:{disable:!0}},loading:{table:{disable:!0}},id:{table:{disable:!0}},errors:{table:{disable:!0}},onBlur:{table:{disable:!0}},onChange:{table:{disable:!0}},rules:{table:{disable:!0}},control:{table:{disable:!0}},name:{table:{disable:!0}},disabled:{table:{disable:!0}},label:{table:{disable:!0}}},tags:["autodocs"]};function g(r){const{variant:o,children:a}=r,{control:c,formState:{errors:n}}=d({mode:"all",defaultValues:{agreement:""}});return t.jsx(u,{children:t.jsx(b,{children:t.jsx(p,{id:"agreement",name:"agreement",control:c,errors:n==null?void 0:n.agreement,variant:o,children:a})})})}const C=r=>{const{variant:o,children:a}=r;return`
+  import RcSesCheckboxFormControl from '@/components/form/inputs/CheckboxFormControl';
+
+  const MyComponent = () => (
+
+  const {
+    control,
+    formState: { errors },
+  } = useForm<FormModel>({
+    mode: 'all',
+    defaultValues: {
+      agreement: '',
+    },
+  })
+
+    <RcSesCheckboxFormControl
+      id='agreement'
+      name='agreement'
+      control={control}
+      errors={errors?.agreement}
+      variant="${o}"
+    >
+     ${a}
+    </RcSesCheckboxFormControl>
+  );`},e={render:r=>t.jsx(g,{...r}),args:{label:"This is label",children:"This is body",variant:"outlined"},parameters:{docs:{source:{type:"dynamic",transform:(r,o)=>C(o.args)}}}};var i,m,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: args => <CheckboxFormControlDemo {...args} />,
+  args: {
+    label: 'This is label',
+    children: 'This is body',
+    variant: 'outlined'
+  },
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic',
+        transform: (_code: string, storyContext: StoryContext) => codeBlock(storyContext.args)
+      }
+    }
+  }
+}`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const Z=["Main"];export{e as Main,Z as __namedExportsOrder,Y as default};
+//# sourceMappingURL=CheckboxFormControl.stories-DShwzYB3.js.map
