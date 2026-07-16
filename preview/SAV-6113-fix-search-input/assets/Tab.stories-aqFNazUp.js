1e75a468de95a727e945e1c5f7d77e505b86b412
saukaite <saukaite@users.noreply.github.com>
2026-07-16 08:36:49 +0000

    deploy: bb65804a9f27e71f5031c34ec771d7fa44bad0ff

---
+++ preview/SAV-6113-fix-search-input/assets/Tab.stories-aqFNazUp.js
@@ -0,0 +1,17 @@
+import{R as b}from"./Tab-COv7QhID.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./createTheme-ClYpPn9r.js";import"./index-CqJgnuY1.js";import"./_commonjsHelpers-gnU0ypJ3.js";import"./generateUtilityClasses-B2ID6taa.js";import"./styled-wPX_n2pi.js";import"./defaultTheme-Ca0qI0zY.js";import"./DefaultPropsProvider-DlaphqLI.js";import"./composeClasses-fLhin0tj.js";import"./ButtonBase-D81Lsw8O.js";import"./useTimeout-DgQ6HlaJ.js";import"./TransitionGroupContext-BmD4SPXa.js";import"./useIsFocusVisible-DPtOLna9.js";const W={title:"components/tabs/Tab",component:b,tags:["autodocs"],argTypes:{disabled:{control:"boolean"},label:{control:"text"},icon:{control:"text"},iconPosition:{control:{type:"select",options:["top","bottom","start","end"]}}}},o={args:{label:"Tab Label"}},e={args:{label:"Home",icon:"⭐",iconPosition:"start"}},r={args:{label:"Disabled Tab",disabled:!0}};var t,a,s;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Tab Label'
+  }
+}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var n,i,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    label: 'Home',
+    icon: '⭐',
+    iconPosition: 'start'
+  }
+}`,...(c=(i=e.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var l,m,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled Tab',
+    disabled: true
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const _=["Default","WithIcon","Disabled"];export{o as Default,r as Disabled,e as WithIcon,_ as __namedExportsOrder,W as default};
+//# sourceMappingURL=Tab.stories-aqFNazUp.js.map
