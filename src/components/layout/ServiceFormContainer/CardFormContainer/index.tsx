ad3060d58e4e1c79d32c641f0d7c86a9bd59d9a9
saukaite <Sandra.Saukaite@registrucentras.lt>
2026-08-13 14:37:31 +0300

    SAV-6509: add CardV2 component

---
+++ src/components/layout/ServiceFormContainer/CardFormContainer/index.tsx
@@ -0,0 +1,72 @@
+import { Container } from '@mui/material'
+
+import RcSesCard from '@/components/common/Card'
+import { RcSesCardProps } from '@/components/common/Card/types'
+import ServiceWizardStepper from '@/components/layout/ServiceWizardStepper/'
+
+import { StepItem } from '../../ServiceWizardStepper/StepperTypes'
+
+export type LayoutMode = 'column' | 'row'
+
+export interface RcSesCardFormContainerProps extends RcSesCardProps {
+  steps: StepItem[]
+  onStepClick?: (index: number) => void
+  activeStep: number
+  layout?: LayoutMode
+  isLoading?: boolean
+}
+
+const RcSesCardFormContainer = ({
+  steps,
+  onStepClick,
+  activeStep,
+  layout,
+  children,
+  isLoading = false,
+  ...cardProps
+}: RcSesCardFormContainerProps) => {
+  const getLayoutDirection = () => {
+    if (layout != null) return layout
+    if (steps.length > 4) {
+      return 'row'
+    }
+    return 'column'
+  }
+
+  const layoutDirection = getLayoutDirection()
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: {
+          xs: 'column',
+          md: getLayoutDirection(),
+        },
+        gap: '24px',
+        paddingBottom: '64px',
+      }}
+    >
+      {steps.length > 2 && (
+        <ServiceWizardStepper
+          steps={steps}
+          onStepClick={onStepClick}
+          activeStep={activeStep}
+          orientation={layoutDirection === 'row' ? 'vertical' : 'horizontal'}
+          isLoading={isLoading}
+        />
+      )}
+
+      <RcSesCard
+        {...cardProps}
+        sx={{
+          width: '100%',
+        }}
+      >
+        {children}
+      </RcSesCard>
+    </Container>
+  )
+}
+
+export default RcSesCardFormContainer
