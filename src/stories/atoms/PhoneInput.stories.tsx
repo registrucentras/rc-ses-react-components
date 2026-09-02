1fcfe6f227625ed68cb36c377e09cd3f23d66e30
saukaite <Sandra.Saukaite@registrucentras.lt>
2026-09-02 16:53:50 +0300

    Reorganise Storybook structure based on Atomic design

---
+++ src/stories/atoms/PhoneInput.stories.tsx
@@ -0,0 +1,111 @@
+import { Meta } from '@storybook/react-vite'
+import { useForm } from 'react-hook-form'
+
+import RcSesPhoneInput from '@/components/form/inputs/PhoneInput'
+import FieldPreviewRow from '@/components/storybook/FieldPreviewRow'
+import FieldView from '@/components/storybook/FieldView'
+import Fields from '@/components/storybook/Fields'
+import PreviewTitle from '@/components/storybook/PreviewTitle'
+
+type FormModel = {
+  phone: string
+  phoneDisabled: string
+  phoneError: string
+}
+
+const meta: Meta<typeof RcSesPhoneInput> = {
+  title: 'Atoms/PhoneInput',
+  component: RcSesPhoneInput,
+  argTypes: {
+    control: {
+      table: {
+        disable: true,
+      },
+    },
+    errors: {
+      table: {
+        disable: true,
+      },
+    },
+    rules: {
+      table: {
+        disable: true,
+      },
+    },
+    name: {
+      table: {
+        disable: true,
+      },
+    },
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+
+function PhoneInputDemo(args: any) {
+  const {
+    control,
+    formState: { errors },
+  } = useForm<FormModel>({
+    mode: 'all',
+    defaultValues: {
+      phone: '',
+      phoneDisabled: '',
+      phoneError: '',
+    },
+  })
+
+  const { label, slotProps } = args
+
+  return (
+    <Fields>
+      <FieldView>
+        <RcSesPhoneInput
+          id='phone'
+          name='phone'
+          control={control}
+          rules={{ required: true }}
+          label={label}
+          errors={errors?.phone}
+          slotProps={slotProps}
+        />
+      </FieldView>
+
+      <FieldPreviewRow>
+        <PreviewTitle>State previews</PreviewTitle>
+        <RcSesPhoneInput
+          id='phoneDisabled'
+          name='phoneDisabled'
+          control={control}
+          rules={{ required: true }}
+          label='Disabled'
+          disabled
+          errors={errors?.phoneDisabled}
+          slotProps={slotProps}
+        />
+        <RcSesPhoneInput
+          id='phoneError'
+          name='phoneError'
+          control={control}
+          rules={{ required: true }}
+          label='Error'
+          errors={{ message: 'Error Message', type: 'required' }}
+          slotProps={slotProps}
+        />
+      </FieldPreviewRow>
+    </Fields>
+  )
+}
+
+export const Main = {
+  render: (args: any) => <PhoneInputDemo {...args} />,
+  args: {
+    label: 'Phone number',
+  },
+}
